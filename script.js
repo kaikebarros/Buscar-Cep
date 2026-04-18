f0fb58ad8c453d63f39d6ed81ca057ec144ae9ee
Kaike Barros <kaikebarros32@gmail.com>
2026-04-18 20:07:45 -0300

    Atualização do site

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 const botao = document.querySelector("#botao");
 const input = document.querySelector("#input");
+const resultado = document.querySelector(".resultado");
+
 botao.addEventListener("click", () => {
   const valorInput = input.value;
   buscarCep(valorInput);
 });
 
 async function buscarCep(valorInput) {
-  const res = await fetch(`https://viacep.com.br/ws/${valorInput}/json/`);
-  const data = await res.json();
-  console.log(data.bairro, data.logradouro, data.localidade);
+  if (!/^\d{8}$/.test(valorInput)) {
+    resultado.textContent = "Digite um CEP válido!";
+    return;
+  }
 
-  const resultado = document.querySelector(".resultado");
-  resultado.textContent = ` ${data.logradouro}, Bairro ${data.bairro} - ${data.localidade}`;
   try {
     const res = await fetch(`https://viacep.com.br/ws/${valorInput}/json/`);
     const data = await res.json();
+
+    if (data.erro) {
+      resultado.textContent = "CEP não encontrado!";
+      return;
+    }
+
+    resultado.textContent = `${data.logradouro}, Bairro ${data.bairro} - ${data.localidade}`;
   } catch (erro) {
-    console.log("Erro ao buscar cep");
+    resultado.textContent = "Erro ao buscar CEP";
+    console.log(erro);
   }
 }
